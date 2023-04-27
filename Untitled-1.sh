905925a94ed0e6f089488cee24b613c0311dcd40
sulecan <bilgicsule@hotmail.com>
2023-04-27 13:56:46 +0200

    Variablen bei Linux

---
+++ Untitled-1.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+echo "The number of arguments passed to the script is: $#"
+echo "The first argument is: $1"
+echo "The second argument is: $2"
+echo "The third argument is: $3"
+
+
+#Bu komut dosyası, bir kullanıcı tarafından çalıştırıldığında, 
+#ekrana toplam argüman sayısını ve her bir argümanın değerini yazdırır.
+
+./myscript.sh hello world 123   # argümanlari da ekleyip calistirirsak
+
+#Çıktı su sekilde olur:
+
+The number of arguments passed to the script is: 3
+The first argument is: hello
+The second argument is: world
+The third argument is: 123
+
+#yani $# =komut dosyasına geçirilen argümanların sayısını belirtir.
+# $1, $2, $3 ise, sırasıyla, birinci, ikinci ve üçüncü argümanlara karsilik value degerlerini belirtir.
